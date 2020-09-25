d17214224009b58643d757dceb66c8f3b5765b9e
EdwardHCodes <38704768+EdwardHCodes@users.noreply.github.com>
2020-09-25 17:36:36 -0500

    python script first commit

---
+++ python_gedcom_reader.py
@@ -0,0 +1,10 @@
+import python-gedcom
+
+tags = ['INDI','NAME','SEX','BIRT','DEAT','FAMC','FAMS','FAM',
+'MARR','HUSB','WIFE','CHIL','DIV','DATE','HEAD','TRLR','NOTE']
+
+levels = ['1','2','3']
+
+gedcom_file = open("Holcomb-Project02.ged", "r")
+
+
