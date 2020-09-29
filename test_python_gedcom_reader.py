79c44a9e82eb3df6b5448478869f80898fc45bbc
EdwardHCodes <38704768+EdwardHCodes@users.noreply.github.com>
2020-09-28 21:48:15 -0500

    Update test_python_gedcom_reader.py

--- test_python_gedcom_reader.py
+++ test_python_gedcom_reader.py
@@ -2,7 +2,7 @@
 The user story:
 A complete look at a user's genealogical history.
 
-These tests are to check the function in the python_gedcom_reader file will accurately count the number of lines, 
+These tests are to check the function in the python_gedcom_reader file will accurately count the number of lines, content of the file,
 """
 
 import unittest
@@ -13,14 +13,16 @@ from python_gedcom_reader import gedcom_file_reader
 class TestGedcomReader(unittest.TestCase):
     def test_order(self):
         self.assertEqual(gedcom_file_reader(gedcom_file_reader), )
-    def test_keywords(self):
+    def test_length(self):
         self.assertEqual(gedcom_file_reader(gedcom_file_reader), 36) 
+    def test_output_format(self):
+        ##Checks the first line of the gedcom file
+        self.assertEqual(gedcom_file_reader(gedcom_file_reader), "<--|NOTE|0|")
+    def test_length_of_gedcom_file(self):
+        self.assertEqual(gedcom_file_reader(gedcom_file_reader), "<--|INDI|1|")
+    def test_length_of_gedcom_file(self):
+        self.assertEqual(gedcom_file_reader(gedcom_file_reader), 36)
     
-    
-
-
-
-
 if __name__ == "__main__":
     unittest.main()
     print('Running unit tests')
